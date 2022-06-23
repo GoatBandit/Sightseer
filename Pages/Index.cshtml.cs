e43c90b921108544fa238be4a5c8b4ea18e43b39
Connor <connoreaster@gmail.com>
2022-06-23 22:57:19 +0100

    Testing

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,11 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace Sightseer.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<Process> Processes { get; set; }
+
+        public string TEST = "ThisIsTheTestVariable.";
+
+        // // SPECIFIC TO WINDOWS
+        // public PerformanceCounter cpuCounter;
+
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -19,7 +28,10 @@ namespace Sightseer.Pages
 
         public void OnGet()
         {
+            var items = Process.GetProcesses().Where(p => !String.IsNullOrEmpty(p.ProcessName) && !p.ProcessName.Contains("handle="));
+            Processes = items.ToList();
 
+            // cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
         }
     }
 }
