d3b2b690bf6169a78d314ae80d31e5d1415443e4
Connor <connoreaster@gmail.com>
2022-06-24 15:10:16 +0100

    No real progress

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System.Diagnostics;
+using System.Threading;
 
 namespace Sightseer.Pages
 {
@@ -13,11 +14,9 @@ namespace Sightseer.Pages
     {
         public List<Process> Processes { get; set; }
 
-        public string TEST = "ThisIsTheTestVariable.";
-
-        // // SPECIFIC TO WINDOWS
-        // public PerformanceCounter cpuCounter;
-
+        // SPECIFIC TO WINDOWS
+        public PerformanceCounter cpuCounter;
+        public string cpuUsage;
 
         private readonly ILogger<IndexModel> _logger;
 
@@ -30,8 +29,34 @@ namespace Sightseer.Pages
         {
             var items = Process.GetProcesses().Where(p => !String.IsNullOrEmpty(p.ProcessName) && !p.ProcessName.Contains("handle="));
             Processes = items.ToList();
+            
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        }
 
-            // cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        public string getCPUUsage()
+        {
+            return cpuCounter.NextValue()+"%";
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void TimerFunction()
+        {
+            int seconds = 1 * 1000;
+            var timer = new Timer(TimerFunc, null, 0, seconds);
+        }
+
+        void TimerFunc(object objectInfo)
+        {
+            Console.WriteLine("Update Call");
         }
     }
 }
